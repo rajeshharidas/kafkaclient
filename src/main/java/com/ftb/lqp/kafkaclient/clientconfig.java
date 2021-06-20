9c2cf652c68b9ce931e9af605bcd77d73ee98efc
rharidas18 <rharidas18@gmail.com>
2021-06-20 11:55:02 -0500

    config

--- src/main/java/com/ftb/lqp/kafkaclient/clientconfig.java
+++ src/main/java/com/ftb/lqp/kafkaclient/clientconfig.java
@@ -1,33 +1,26 @@
 package com.ftb.lqp.kafkaclient;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
 @Configuration
 public class clientconfig {
 
 	@Bean
 	public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
-		ObjectMapper om = new ObjectMapper();
-		om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-		om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-		// redis serialize
-		Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-		jackson2JsonRedisSerializer.setObjectMapper(om);
-		StringRedisTemplate template = new StringRedisTemplate(factory);
-		template.setValueSerializer(jackson2JsonRedisSerializer);
-		template.setHashKeySerializer(jackson2JsonRedisSerializer);
-		template.setHashValueSerializer(jackson2JsonRedisSerializer);
-		template.setValueSerializer(jackson2JsonRedisSerializer);
-		template.afterPropertiesSet();
-		return template;
+		 RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+		    redisTemplate.setKeySerializer(new StringRedisSerializer());
+		    redisTemplate.setValueSerializer(new GenericToStringSerializer<>(String.class));
+		    // avoids proxy
+		    redisTemplate.setExposeConnection(true);
+		    redisTemplate.setConnectionFactory(factory);
+		    redisTemplate.afterPropertiesSet();
+		    return redisTemplate;
 	}
+	
 
 }
